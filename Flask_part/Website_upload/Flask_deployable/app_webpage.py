c88eab526b3339daf3727319609c5b92a4c3a5de
vermavinay <vermavinay982@gmail.com>
2020-09-12 22:44:08 +0530

    added face recog

--- Flask_part/Website_upload/Flask_deployable/app_webpage.py
+++ Flask_part/Website_upload/Flask_deployable/app_webpage.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from werkzeug.utils import secure_filename
+from detect_glasses import get_details
+
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
 	a = url_for('static', filename= 'img/vislogo.png')
 	aa = url_for('static', filename= 'img/img_lights.jpg')
@@ -10,12 +13,43 @@ def index():
 	l=[]
 	al=[]
 	cat_l=[]
-	dict = {'phy':50,'che':60,'maths':70, 'CSE':95}
+	data_dict = {}
+	
 	for i in range(19):
 		l.append(a)
 		al.append(aa)
 		cat_l.append(ca)
-	return render_template('index.html', img_list=l, all_list=al, result=dict, cat_list=cat_l)
+
+	if request.method == 'POST':
+		f = request.files['file']
+		name = f.filename
+		print(name)
+		f.save(secure_filename(f.filename))
+		li = get_details(name)
+		print(li)
+		data_dict = {}
+		for i in li:
+			data_dict[i[0]]=i[1]
+
+		# x = [str(i[1]) for i in li]
+		# txt = '\n'.join(x)
+		# return f'{txt}file uploaded successfully'
+
+	return render_template('index.html', img_list=l, all_list=al, result=data_dict, cat_list=cat_l)
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      name = f.filename
+      print(name)
+      f.save(secure_filename(f.filename))
+      li = get_details(name)
+      print(li)
+      x = [str(i[1]) for i in li]
+      txt = '\n'.join(x)
+      return f'{txt}file uploaded successfully'
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=3000,debug=True)
